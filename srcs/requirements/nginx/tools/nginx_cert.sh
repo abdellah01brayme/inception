45b3f92a82bec3360e793ec68581da9a742e3707
aid-bray <aid-bray@aid-bray.aid-bray.42.fr>
2026-09-17 07:24:56 -0400

    add nginx

---
+++ srcs/requirements/nginx/tools/nginx_cert.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+set -e
+
+mkdir -p /etc/ssl/nginx/private &&\
+mkdir -p /etc/ssl/nginx/certs
+
+openssl req -x509 -nodes -days 365 \
+	-newkey rsa:2048 \
+	-keyout /etc/ssl/nginx/private/aid-bray.key \
+	-out /etc/ssl/nginx/certs/aid-bray.crt \
+	-subj "/C=MA/ST=RA/L=RA/O=42/OU=42/CN=aid-bray.42.fr"
+
+nginx -t
+
+echo "Starting Nginx in foreground..."
+
+exec nginx -g "daemon off;"
